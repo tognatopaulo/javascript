619e193f06f020fd0d6272447d25d3d6a1d0e77a
tognatopaulo <paulo.tognato@hotmail.com>
2023-07-31 12:04:51 -0300

    Aula3

--- 2708-node-lib-md/index.js
+++ 2708-node-lib-md/index.js
@@ -1,26 +1,28 @@
-import fs from "fs";
-import chalk from "chalk";
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados;
+}
 
 function trataErro(erro) {
-    console.log(erro)
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-// async / await
-async function pegaArquivo(caminhoDoArquivo){
-    try{
-        const encoding = 'utf-8'
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+// async/await
 
-    } catch(erro) {
-        trataErro(erro)
-    }
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
 
-pegaArquivo('2708-node-lib-md/arquivos/texto.md')
-pegaArquivo('2708-node-lib-md/arquivos/')
-
-
-
-
+pegaArquivo('./arquivos/texto.md');
